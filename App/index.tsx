53d0cec57e7cf1e8a661972703664b1b197d0a68
default-INT <55191884+default-INT@users.noreply.github.com>
2021-06-18 13:44:14 +0300

    Create structure

---
+++ App/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+
+const App = () => {
+  return (
+    <View>
+      <Text>App TSX</Text>
+    </View>
+  )
+};
+
+
+export default App;
